5685fe3a81723725bb5eeb3ab93e2676ce073c9e
Marvin Martinez <marvinmartinez579@gmail.com>
2017-01-27 13:06:49 -0600

    first commit

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request
+import pg
+
+app = Flask('todo-list')
+db = pg.DB(dbname='todo_list_db', host="localhost",
+           user="postgres", passwd="rocket")
+
+
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
+
+@app.route('/tasks')
+def list_tasks():
+    results = db.query('select * from task order by id').dictresult()
+    return jsonify(results)
+
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    description = request.form.get('task')
+    results = db.insert('task', description=description)
+    return jsonify(results)
+
+
+@app.route('/mark_task', methods=['POST'])
+def mark_task():
+    id = request.form.get('id')
+    done = request.form.get('done')
+    result = db.update('task', {'id': id, 'done': done})
+    return jsonify(result)
+
+
+@app.route('/remove_complete', methods=['POST'])
+def remove_complete():
+    result = db.query('delete from task where done = true')
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/todo.js
@@ -0,0 +1,43 @@
+$(function() {
+    $.get('/tasks', updateList);
+
+    // Write code here
+    $("#form").keypress(function(event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            console.log(event.keyCode);
+            var data = $("#new-task").serialize();
+            $.post('/add_task', data, function() {});
+            $("ul").empty();
+            $.get('/tasks', updateList);
+            $("#new-task").val("")
+        };
+    });
+    $(document).on('click', '#check', function() {
+        var task_done = $(this).prop("checked");
+        var task_id = $(this).parent().attr("id");
+        var data = {
+            id: task_id,
+            done: task_done
+        };
+        $.post('/mark_task', data, function() {
+            if (data.done === true) {
+                $("#" + data.id).addClass("done")
+            } else if (data.done === false) {
+                $("#" + data.id).removeClass("done")
+            }
+        });
+    });
+    $('#remove-completed').on('click', function(){
+        console.log("remove completed");
+        $.post('/remove_complete');
+        $("ul").empty();
+        $.get('/tasks', updateList);
+    });
+});
+var updateList = function(resultsArr) {
+    resultsArr.forEach(function(results) {
+        var html = "<li id='" + results.id + "'>" + results.description + "<input id='check' type='checkbox'></li>"
+        $("#task-list").append(html)
+    });
+};
